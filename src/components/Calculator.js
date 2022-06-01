c385c7ab2780508d5c2fd74a11c2329f5b313ed5
sarazaToussaint <tmsaraza@gmail.com>
2022-06-01 11:42:54 +0200

    Fix the layout error

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -7,34 +7,34 @@ class Calculator extends React.Component {
       <div className="container">
         <div className="display-result">0</div>
         <div className="row-one grid">
-          <button type="button" className="col">Ac</button>
-          <button type="button" className="col">+/-</button>
-          <button type="button" className="col">%</button>
-          <button type="button" className="col">/</button>
+          <button type="button" className="btn">Ac</button>
+          <button type="button" className="btn">+/-</button>
+          <button type="button" className="btn">%</button>
+          <button type="button" className="btn">/</button>
         </div>
         <div className="row-two grid">
-          <button type="button" className="col">7</button>
-          <button type="button" className="col">8</button>
-          <button type="button" className="col">9</button>
-          <button type="button" className="col">*</button>
+          <button type="button" className="btn">7</button>
+          <button type="button" className="btn">8</button>
+          <button type="button" className="btn">9</button>
+          <button type="button" className="btn">*</button>
         </div>
         <div className="row-three grid">
-          <button type="button" className="col">4</button>
-          <button type="button" className="col">5</button>
-          <button type="button" className="col">6</button>
-          <button type="button" className="col">-</button>
+          <button type="button" className="btn">4</button>
+          <button type="button" className="btn">5</button>
+          <button type="button" className="btn">6</button>
+          <button type="button" className="btn">-</button>
         </div>
         <div className="row-four grid">
-          <button type="button" className="col">1</button>
-          <button type="button" className="col">2</button>
-          <button type="button" className="col">3</button>
-          <button type="button" className="col">+</button>
+          <button type="button" className="btn">1</button>
+          <button type="button" className="btn">2</button>
+          <button type="button" className="btn">3</button>
+          <button type="button" className="btn">+</button>
         </div>
         <div className="row-five grid">
           <div type="button" className="col div-col" />
-          <button type="button" className="col">0</button>
-          <button type="button" className="col">.</button>
-          <button type="button" className="col">=</button>
+          <button type="button" className="btn">0</button>
+          <button type="button" className="btn">.</button>
+          <button type="button" className="btn">=</button>
         </div>
       </div>
     );
