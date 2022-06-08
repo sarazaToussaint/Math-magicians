24b6b1a77e3cabded66835b999c6b099959a114b
sarazaToussaint <tmsaraza@gmail.com>
2022-06-08 17:53:12 +0200

    Add the home page,  Quote page and layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Calculator from './components/Calculator';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Quote from './pages/Quote';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Calculator />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/quote" element={<Quote />} />
+        </Routes>
       </div>
     );
   }
