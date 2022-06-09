8ad9500b6aec86abd9007cd5c9c9396052e8d759
medsonmoombe <medsonmoombe21@gmail.com>
2022-06-09 17:21:46 +0200

    Add the App test file and the test function

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Home = () => (
   <section className="home">
-    <h2 className='heading'>Welcome to our page!</h2>
-    <p className='para'>
+    <h2>Welcome to our page!</h2>
+    <p>
       Lorem Ipsum is simply dummy text of the printing and typesetting industry.
       Lorem Ipsum has been the industry standard dummy text ever since the 1500s,
       when an unknown printer took a galley of type and scrambled it to make a
